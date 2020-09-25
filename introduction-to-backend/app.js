a21048dc171b83c67fed700f9b0dc18230636b12
Chuks Emmanuel <chuksdevinfo@gmail.com>
2020-09-25 15:42:36 +0100

    add ejs, mongoose, create post, read posts, show post in ui

--- introduction-to-backend/app.js
+++ introduction-to-backend/app.js
@@ -1,15 +1,57 @@
+const { urlencoded } = require("express");
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 //Set view engine
 app.set("view engine", "ejs");
 
+// Use body-parser
+app.use(express.urlencoded({ extended: true }));
+
 // Use Pubic directory
 app.use(express.static("public"));
 
+//Connect mongoose to mongodb
+mongoose.connect("mongodb://localhost:27017/snapblog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Create mongoose schema
+const postSchema = mongoose.Schema({
+  title: {
+    type: String,
+    // required: true,
+  },
+  imgUrl: {
+    type: String,
+    default:
+      "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  },
+  content: {
+    type: String,
+    // required: true,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+//Create mongoose model
+const Post = mongoose.model("Post", postSchema);
+
 // Routes
 app.get("/", (req, res) => {
-  res.render("home");
+  Post.find({})
+    .then((posts) => res.render("home", { posts: posts }))
+    .catch((err) => console.log(err));
 });
 
 app.get("/about", (req, res) => {
@@ -22,8 +64,25 @@ app.get("/contact", (req, res) => {
 app.get("/create-post", (req, res) => {
   res.render("create-post");
 });
-app.get("/post", (req, res) => {
+app.get("/single-post", (req, res) => {
   res.render("single-post");
+});
+
+// POST ROUTES
+app.post("/create-post", (req, res) => {
+  const newPost = new Post({
+    title: req.body.title,
+    imgUrl: req.body.img,
+    content: req.body.content,
+    featured: req.body.featured === "on" ? true : false,
+  });
+
+  newPost
+    .save()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+
+  res.redirect("/");
 });
 
 app.get("*", (req, res) => {
